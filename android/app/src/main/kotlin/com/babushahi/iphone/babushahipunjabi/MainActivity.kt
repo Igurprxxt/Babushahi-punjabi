e993741de6770420c57ca7ac8326cf5ab75e4731
Gurpreet Singh <Gurpreet.singh@codekraftsolutions.com>
2025-01-24 22:51:25 +0530

    android stable build

--- android/app/src/main/kotlin/com/babushahi/iphone/babushahipunjabi/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.babushahi.iphone.babushahipunjabi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/tirchhinazarmedia/babushahipunjabi/MainActivity.kt
+++ android/app/src/main/kotlin/com/tirchhinazarmedia/babushahipunjabi/MainActivity.kt
@@ -2,4 +2,5 @@ package com.babushahi.punjabi
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
