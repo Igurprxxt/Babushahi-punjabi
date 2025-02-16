defa6a08e64db9206ee12e4b315ad9b8e0af5982
Gurpreet Singh <Gurpreet.singh@codekraftsolutions.com>
2025-02-16 09:50:49 +0530

    resolved punjabi android issue not sharing whatsapp

--- lib/screens/web_view_screen.dart
+++ lib/screens/web_view_screen.dart
@@ -93,11 +93,11 @@ class _FullNewsState extends State<FullNews> {
   }
 
   _launchURL(Uri url) async {
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url, mode: LaunchMode.externalApplication);
-    } else {
-      throw 'Could not launch $url';
-    }
+    // if (await canLaunchUrl(url)) {
+    await launchUrl(url, mode: LaunchMode.externalApplication);
+    // } else {
+    //   throw 'Could not launch $url';
+    // }
   }
 
   @override
